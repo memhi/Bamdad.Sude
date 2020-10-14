75860dbd65ac1a101f75807ab94d0b029c561ff3
Younas Sahraee <sahraee@live.com>
2020-10-14 11:51:29 +0330

    Update Packages

---
+++ Sude.Domain/Interfaces/IOrderRepository.cs
@@ -0,0 +1,27 @@
+﻿using Sude.Domain.Models.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sude.Domain.Interfaces
+{
+    public interface IOrderRepository
+    {
+        Task<IEnumerable<Order>> GetOrdersAsync();
+        IEnumerable<Order> GetOrders();
+      //  bool IsExistServing(string Title);
+
+        bool AddOrder(Order order);
+        bool EditOrder(Order order);
+        bool DeleteOrder(Guid servingId);
+
+        Task<Order> GetOrderByIdAsync(Guid orderId);
+        Order GetOrderById(Guid orderId);
+       
+
+        void Save();
+        Task SaveAsync();
+    }
+}
