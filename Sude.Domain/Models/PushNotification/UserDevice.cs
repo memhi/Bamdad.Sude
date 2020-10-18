29864d38a8d7ac9286479d10db36ce69d48b5e4d
Younas Sahraee <sahraee@live.com>
2020-10-18 14:59:04 +0330

    Update Packages

---
+++ Sude.Domain/Models/PushNotification/UserDevice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Sude.Domain.Models.Common;
+
+namespace Sude.Domain.Models.PushNotification
+{
+    public class UserDevice : BaseModel<Guid>
+    {
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public string PushEndpoint { get; set; }
+        public string PushP256DH { get; set; }
+        public string PushAuth { get; set; }
+
+    }
+
+}
